33821a527015ceea62987bea07bed814c525206a
Manuel Walter Navarro Z <manuel08n@gmail.com>
2023-07-27 14:47:07 -0500

    development google_fonts dependencies

---
+++ lib/config/helper/app_text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyleDetailComics {
+  TextStyle textStyleDetails(Color color) => GoogleFonts.lato(
+    color: color,
+    fontSize: 16
+  );
+}
